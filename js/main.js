866c411ecbe5b25353d32fb439fea5eda185e866
dargavavid <dargavavid@gmail.com>
2017-12-27 18:39:03 +0100

    Add function to fetch and set dictionary

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 const app = {
     canvas: document.querySelector("#canvas"),
     ctx: this.canvas.getContext("2d"),
+    dict: null
 };
 
+function fetchAndSetDictionary() {
+    fetch("../js/dict.json").then(response => response.json()).then(json => app.dict = json).catch(err => err.message);
+}
+
+
+fetchAndSetDictionary();
