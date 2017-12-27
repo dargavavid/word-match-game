08ecc2a61c9f6f039f2c57f666552868128c6cee
dargavavid <dargavavid@gmail.com>
2017-12-27 18:51:34 +0100

    Add Word class and basic fall method

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+class Word {
+    constructor(word, x, y, vy) {
+        this.word = word;
+        this.x = x;
+        this.y = y;
+        this.vy = vy;
+    }
+    fall() {
+        this.y -= this.vy;
+    }
+}
+
 const app = {
     canvas: document.querySelector("#canvas"),
     ctx: this.canvas.getContext("2d"),
@@ -8,10 +20,11 @@ function fetchAndSetDictionary() {
     fetch("../js/dict.json").then(response => response.json()).then(json => app.dict = json).catch(err => err.message);
 }
 
-
 function getRandomWord() {
     const words = Object.keys(app.dict), roll = Math.floor(Math.random() * words.length);
     return words[roll];
 }
+
+
 
 fetchAndSetDictionary();
